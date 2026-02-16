5e3b85f911e37d318fea65fab7e7d1ea7f4d410b
abhirawat03 <abhirawat8076@gmail.com>
2026-02-16 23:54:41 +0530

    add refresh token functionality and fix token handling in auth middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.js";
 import {upload} from "../middlewares/multer.js"
 import { verifyJwt } from "../middlewares/auth.js";
 
@@ -23,5 +23,7 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJwt ,logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
