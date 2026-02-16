8b0435ceb5692b020eb4570475b02c5ae6106f21
abhirawat03 <abhirawat8076@gmail.com>
2026-02-16 21:43:04 +0530

    add access token ,refresh token , login, logout

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+import { User } from "../models/user.js";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+
+export const verifyJwt = asyncHandler(async(req,res,next)=>{
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","" )
+
+    if(!token){
+        throw new ApiError(401,"unauthorized request")
+    }
+
+    const decodedToken = jwt.verify(token, proccess.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+    if(!user){
+        //TODO: discuss about frontend
+        throw new ApiError(401,"Invalid Access Token")
+    }
+
+    req.user = user;
+    next()
+
+})
