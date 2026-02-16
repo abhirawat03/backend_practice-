0757ad7cf34675b795029d5e7316739c13e2a436
abhirawat03 <abhirawat8076@gmail.com>
2026-02-16 17:16:00 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import {app} from "./app.js"
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000),()=>{
-        console.log(`Server is running at port : ${process.env.PORT}`)
-    }
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`⚙️  Server is running at port : ${process.env.PORT}`)
+    })
 })
 .catch((err) => {
     console.log("MONGODB connection failed !!!", err);
